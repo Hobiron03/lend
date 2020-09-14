e9391576d55594bd529ba43606054fa5d2559b61
obatakyoukan <oba-kyoukan@eis.hokudai.ac.jp>
2020-09-14 15:34:13 +0900

    modify

--- README.md
+++
@@ -1,2 +0,0 @@
-# Rule
-- Issue3 のとき branch名 feature/#3
--- lend_frontend/src/components/Login/Login.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Login = () => {
-  return (
-    <div>
-      <p>Here is Login</p>
-    </div>
-  );
-};
