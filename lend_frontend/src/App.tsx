c301095c5aaae5c1ff4f7712ae3a79c6a100a202
HOBIRON <yudainepnep@icloud.com>
2020-09-14 17:40:37 +0900

    #28 Install Material-UI

--- lend_frontend/src/App.tsx
+++ lend_frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Login } from "./components/Login/Login";
+import Login from "./components/Login/Login";
 import { TabBar } from "./components/organizations/TabBar/TabBar";
 import AppBar from "./components/organizations/AppBar/AppBar";
 import BottomBar from "./components/organizations/BottomBar/BottomBar";
@@ -9,10 +9,10 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <TabBar/>
-      <AppBar/>
+      <TabBar />
+      <AppBar />
       <Login />
-      <BottomBar/>
+      <BottomBar />
     </div>
   );
 }
