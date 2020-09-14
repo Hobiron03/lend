0912225d9f63c1cac0f4de1075aa58068906e979
HOBIRON <yudainepnep@icloud.com>
2020-09-14 16:26:27 +0900

    #14 Create BookCard Component

--- lend_frontend/src/App.tsx
+++ lend_frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Login } from "./components/Login/Login";
+import BookCard from "./components/BookCard/BookCard";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
