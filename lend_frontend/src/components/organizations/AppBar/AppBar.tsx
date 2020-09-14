c201bfc3ed169361f4841acd136f638cac071ff8
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-14 15:38:15 +0900

    AppBarを実装

---
+++ lend_frontend/src/components/organizations/AppBar/AppBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+export const AppBar = () => {
+	
+	const onClickNotification = (e: React.MouseEvent<HTMLInputElement>) => {
+		console.log('通知ページへ遷移');
+	}
+
+	const onClickSettings = (e: React.MouseEvent<HTMLInputElement>) => {
+		console.log('設定ページへ遷移');
+	}
+	
+	return (
+		<header className="appBar">
+			<p>RENT</p>
+			<div className="action-buttons">
+				<div className="action-button" onClick={onClickNotification}>
+					<NotificationsIcon/>
+				</div>
+			</div>
+		</header>
+	)
+}
