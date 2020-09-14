161525b25477e1c3510a965f656ffc1d6d8a4ed6
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-14 15:04:49 +0900

    material-iconを導入

--- lend_frontend/src/App.tsx
+++ lend_frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Login } from "./components/Login/Login";
+import AppBar from "./components/organizations/AppBar"
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <AppBar/>
       <Login />
     </div>
   );
---
+++ lend_frontend/src/components/organizations/AppBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+export default function AppBar() {
+	return (
+		<div>
+			<header>
+				<p>RENT</p>
+				<div>
+					<NotificationsIcon/>
+				</div>
+			</header>
+		</div>
+	)
+}
