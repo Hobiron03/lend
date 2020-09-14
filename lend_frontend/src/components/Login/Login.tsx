754a6c601c77b5c0038bc16a6ba3b8d64cf81ae1
daomo <m401shima@gmail.com>
2020-09-14 16:55:21 +0900

    fix

--- lend_frontend/src/components/Login/Login.tsx
+++ lend_frontend/src/components/Login/Login.tsx
@@ -1,39 +1,24 @@
 import React, { useState } from "react";
 
-export const Login = () => {
+const Login = () => {
   const [registerFlag, setRegisterFlag] = useState(false);
 
   const toggleLoginAndRegister = () => {
     setRegisterFlag(!registerFlag);
   };
-
-  if (registerFlag) {
-    return (
-      <div className="login">
-        <h1>レント Lento!</h1>
-        <h2>新規登録</h2>
-        <form>
-          <div><input type="text" className="login-form login-form-username" placeholder="ユーザー名" /></div>
-          <div><input type="password" className="login-form login-form-password" placeholder="パスワード" required pattern="[A-Za-z0-9]*" /></div>
-          <p className="to-registration" onClick={() => toggleLoginAndRegister()}>ログイン</p>
-          <input type="submit" className="login-form-submit" value="新規登録" />
-        </form>
-      </div>
-    )
-  } else {
-    return (
-      <div className="login">
-        <h1>レント Lento!</h1>
-        <h2>ログイン</h2>
-        <form>
-          <div><input type="text" className="login-form login-form-username" placeholder="ユーザー名" /></div>
-          <div><input type="password" className="login-form login-form-password" placeholder="パスワード" required pattern="[A-Za-z0-9]*" /></div>
-          <p className="to-registration" onClick={() => toggleLoginAndRegister()}>ユーザー登録</p>
-          <input type="submit" className="login-form-submit" value="ログイン" />
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className="login">
+      <h1>レント Lento!</h1>
+      <h2>{registerFlag ? "新規登録" : "ログイン"}</h2>
+      <form>
+        <div><input type="text" className="login-form login-form-username" placeholder="ユーザー名" /></div>
+        <div><input type="password" className="login-form login-form-password" placeholder="パスワード" required pattern="[A-Za-z0-9]*" /></div>
+        <p className="to-registration" onClick={() => toggleLoginAndRegister()}>{registerFlag ? "ログイン" : "新規登録"}</p>
+        <input type="submit" className="login-form-submit" value={registerFlag ? "新規登録" : "ログイン"} />
+      </form>
+    </div>
+  )
 
 };
 
+export default Login;
