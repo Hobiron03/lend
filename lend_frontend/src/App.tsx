1035f8c692b2bfe5a96b7962fd65b75830b7105c
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-15 10:07:42 +0900

    Screenコンポーネントを作成

--- lend_frontend/src/App.tsx
+++ lend_frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Login } from "./components/Login/Login";
+import Login from "./components/Login/Login";
+import Screen from "./components/Screen/Screen";
 import { TabBar } from "./components/organizations/TabBar/TabBar";
 import AppBar from "./components/organizations/AppBar/AppBar";
 import BottomBar from "./components/organizations/BottomBar/BottomBar";
@@ -9,10 +10,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <TabBar/>
-      <AppBar/>
       <Login />
-      <BottomBar/>
     </div>
   );
 }
