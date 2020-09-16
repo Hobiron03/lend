cace11580c15bd19ed07f5df18c63f046a4b04c2
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-16 18:06:55 +0900

    タブの実装

--- lend_frontend/src/components/MyBookList/MyBookList.tsx
+++ lend_frontend/src/components/MyBookList/MyBookList.tsx
@@ -1,30 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Screen from "../Screen/Screen";
+import TabBar from "../organizations/TabBar/TabBar";
 import BookCard from "../BookCard/BookCard";
 import Book from "../../model/book";
 
 const ENTRY_POINT = process.env.REACT_APP_API_ENTRYPOINT;
+type MyBookShellTabState = "available" | "lending";
+const initState = "available";
 
-// NOTE: エラーハンドリング
 const MyBookList = () => {
-
 	const [mybooks, setMyBooks] = useState<Book[]>([]);
+	const [showBooks, setShowBooks] = useState<Book[]>([])
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		axios.get(ENTRY_POINT + '/books?user_id=1').then((res) => {
-			const books = res.data.map((data: any) => Book.fromJson(data));
+			const books: Book[] = res.data.map((data: any) => Book.fromJson(data));
 			setMyBooks(books);
-			console.log(books);
+			if (initState === "available") {
+				setShowBooks(books);
+			} else {
+				setShowBooks(books.filter(({ status }) => status === "lending"));
+			}
+			setIsLoading(false);
 		});
-	}, [])
+	}, []);
+
+	const handleTabChange = (tabState: MyBookShellTabState) => {
+		if(tabState === "available"){
+			setShowBooks(mybooks);
+		}else{
+			setShowBooks(mybooks.filter(({ status }) => status === "lending"));
+		}
+	}
 
 	return (
 		<Screen>
+			<TabBar initState={initState} onTabChange={handleTabChange}/>
 			<div className="mybooklist-body">
 			{
-				mybooks.map(mybook =>
-					<BookCard key={mybook.id} book={mybook}/>
+				showBooks.length === 0 ? !isLoading && (
+					<div className="empty-message">
+						対象の本はありません
+					</div>
+				):(
+					showBooks.map(mybook =>
+						<BookCard key={mybook.id} book={mybook}/>
+					)
 				)
 			}
 			</div>
