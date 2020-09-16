3623ff1cda022606547ab9edf35a976e619553e8
HOBIRON <yudainepnep@icloud.com>
2020-09-16 12:05:10 +0900

    #7 create lend logic in select friend

--- lend_frontend/src/components/SelectFriendScreen/SelectFriendScreen.tsx
+++ lend_frontend/src/components/SelectFriendScreen/SelectFriendScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import FriendColumn from "./FriendColumn/FriendColumn";
 import BaseModal from "@material-ui/core/Modal";
@@ -6,42 +6,67 @@ import ModalContentConfirm from "../Modal/ModalContentConfirm/ModalContentConfir
 import friends from "./Friends.json";
 import Screen from "../Screen/Screen";
 import "./_SelectFriendScreen.scss";
+import axios from "axios";
 interface SelectFriendScreenProps extends RouteComponentProps<{ id: string }> {
   lendBookName: string;
-  //貸し出す本の情報が欲しい？
 }
+
+const ENTRY_POINT = process.env.REACT_APP_API_ENTRYPOINT;
 
 const SelectFriendScreen = (props: SelectFriendScreenProps) => {
   const [searchText, setSearchText] = useState<string>("");
   const [isSearch, setIsSearch] = useState<boolean>(false);
-  const [isModalOpen, setModalOpen] = useState<boolean>(true);
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  const [lendUsername, setLendUsername] = useState<string>("");
+
+  const messagesDomRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const y =
+      messagesDomRef.current!.scrollHeight -
+      messagesDomRef.current!.clientHeight;
+    messagesDomRef.current!.scrollTo(0, y);
+  });
 
   const handleSearch = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
     e.target.value === "" ? setIsSearch(false) : setIsSearch(true);
   }, []);
 
-  const handleModalOpen = () => {
+  const handleModalOpen = (friendName: string) => {
+    setLendUsername(friendName);
     setModalOpen(true);
   };
 
-  const handleClose = () => {
+  const handleModalClose = () => {
     setModalOpen(false);
   };
 
-  const onConfirm = () => {
+  const onConfirm = async () => {
     //貸し出しのAPIを叩く
-    console.log("貸し出しのAPIを叩く");
+    await axios
+      .post(ENTRY_POINT + "/lend", {
+        //自分のユーザーIDを
+        id: 1,
+        borrower_id: 1,
+        book_id: props.match.params.id,
+        deadline: "2020/09/22 12:26:48.084076",
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    console.log("貸し出しのAPIを叩くいてモーダルを閉じる");
   };
 
   const onDeny = () => {
     //貸し出しを中止してモーダルを閉じる
     console.log("貸し出しを中止してモーダルを閉じる");
+    handleModalClose();
   };
 
   return (
     <Screen>
-      <div className="select_friend_screen">
+      <div className="select_friend_screen" ref={messagesDomRef}>
         <div className="select_friend_screen__top">
           <p className="select_friend_screen__top__title">
             {props.lendBookName}
@@ -61,7 +86,7 @@ const SelectFriendScreen = (props: SelectFriendScreenProps) => {
           {friends.friends.map((friend) => {
             if (isSearch) {
               return searchText === friend.name ? (
-                <span onClick={handleModalOpen}>
+                <span onClick={() => handleModalOpen(friend.name)}>
                   <FriendColumn
                     key={friend.id}
                     name={friend.name}
@@ -73,24 +98,28 @@ const SelectFriendScreen = (props: SelectFriendScreenProps) => {
               );
             } else {
               return (
-                <FriendColumn
-                  key={friend.id}
-                  name={friend.name}
-                  iconImageURL={friend.iconImageURL}
-                />
+                <span onClick={() => handleModalOpen(friend.name)}>
+                  <FriendColumn
+                    key={friend.id}
+                    name={friend.name}
+                    iconImageURL={friend.iconImageURL}
+                  />
+                </span>
               );
             }
           })}
         </div>
       </div>
+
       <BaseModal
         open={isModalOpen}
+        onClose={handleModalClose}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
         <div className="modal_background">
           <ModalContentConfirm
-            description="鬼滅の刃一巻を貸し出しても良いですか"
+            description={`${props.lendBookName} を ${lendUsername} さんに貸し出しても良いですか?`}
             onConfirm={onConfirm}
             onDeny={onDeny}
           />
