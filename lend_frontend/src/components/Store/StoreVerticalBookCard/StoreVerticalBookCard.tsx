9be8e5c9ac2622d7ccb8d0e99dbef78128b645c9
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-16 12:02:19 +0900

    fix

--- lend_frontend/src/components/Store/StoreVerticalBookCard/StoreVerticalBookCard.tsx
+++ lend_frontend/src/components/Store/StoreVerticalBookCard/StoreVerticalBookCard.tsx
@@ -5,8 +5,14 @@ type Props = {
 	book: Book,
 };
 const StoreVerticalBookCard = ({ book }: Props) => {
+
+	const handleBuy = () => {
+		// TODO: APIコール実装
+		console.log('購入');
+	}
+
 	return (
-		<div className="store-book-card">
+		<div className="store-book-card" onClick={handleBuy}>
 			<img src={book.image} height="150" width="120" />
 			<div className="title">{book.name}</div>
 			<div className="auther">{book.auther}</div>
