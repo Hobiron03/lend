c106e552b43749eff71514e909f7fdceac5c4afc
HOBIRON <yudainepnep@icloud.com>
2020-09-16 10:17:56 +0900

    #27 fix App.tsx div className

--- lend_frontend/src/App.tsx
+++ lend_frontend/src/App.tsx
@@ -7,15 +7,12 @@ import {
 } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Screen from "./components/Screen/Screen";
-import { TabBar } from "./components/organizations/TabBar/TabBar";
-import AppBar from "./components/organizations/AppBar/AppBar";
-import BottomBar from "./components/organizations/BottomBar/BottomBar";
 import "./App.scss";
 import Viewer from "./components/Viewer/Viewer";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Router>
         <Switch>
           {/* ログイン/ユーザー登録ページ */}
