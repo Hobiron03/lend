8456785aba9eda4af0b636fd6548687f0f82c1b8
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-09-16 11:56:37 +0900

    Revert "ストア画面を実装"
    This reverts commit c4648526255f82abcfd82849e0915ea6b07ff8d6.

--- lend_frontend/src/components/BookCard/BookCard.tsx
+++ lend_frontend/src/components/BookCard/BookCard.tsx
@@ -5,75 +5,40 @@ import "./Button/Button";
 import Button from "./Button/Button";
 import Book from '../../model/book';
 
-/**
- * 本の状態によってボタンを出し分けるためのCardType
- * + read_lend: 読むボタンと貸すボタンを設置（今すぐ読める本）
- * + buy: 購入ボタンのみ設置
- * + read_lend_return: 読むボタン、買うボタン、返すボタンを設置
- * + lending_label: 貸し出しボタンを設置
- */
-type CardType = "read_lend" | "buy" | "read_buy_return" | "lending_label";
-
 interface BookCardProps {
   book: Book,
-  type?: CardType,
 }
 
-const BookCard = ({ book, type = "read_lend" }: BookCardProps): JSX.Element => {
+const BookCard = (props: BookCardProps): JSX.Element => {
   const history = useHistory();
 
   const handleRead = () => {
-    history.push(`/mybook/${book.id}/read`);
-  }
-
-  // 購入の場合のみ、カードをクリックしてアクションを起こせる
-  const handleBuy = () => {
-    if(type === "buy"){
-      console.log("購入")
-    }
+    history.push(`/mybook/${props.book.id}/read`);
   }
 
   return (
-    <div className="BookCard" onClick={handleBuy}>
+    <div className="BookCard">
       <div className="BookCard__top">
         <div className="BookCard__top__left">
           <div className="BookCard__top__left__image">
-            <img src={book.image} alt="cover" />
+            {/* TODO: バックエンドで表紙画像が未実装なのでとりあえず1ページ目を表示します。（現時点9/15 16:10 ではurlがミスってるっぽくてそもそも画像が見れませんが。） */}
+            <img src={props.book.image} alt="cover" />
           </div>
         </div>
         <div className="BookCard__top__right">
           <div className="BookCard__top__right__description">
             <p className="BookCard__top__right__description__title">
-              {book.name}
+              {props.book.name}
             </p>
-            <p>{book.info}</p>
+            {/* TODO: バックエンドでdescriptionが未実装なので書籍説明が未実装です */}
+            <p>{props.book.name.repeat(4)}</p>
           </div>
         </div>
       </div>
-      {/* 購入以外は、フッターにボタンを表示する */}
-      {
-        type !== "buy" && (
-          <div className="BookCard__under">
-            {
-              type === "read_lend" ? (
-                <>
-                  <Button content="読む" onClick={handleRead} />
-                  <Button content="貸す" onClick={() => console.log("貸す")} />
-                </>
-              ) : type === "read_buy_return" ? (
-                <>
-                  <Button content="読む" onClick={handleRead} />
-                  <Button content="購入" onClick={() => console.log("購入")} />
-                  <Button content="貸す" onClick={() => console.log("貸す")} />
-                </>
-              ) : type === "lending_label" && (
-                // TODO: デザイン未確認
-                <div>貸し出し中です</div>
-              )
-            }
-          </div>
-        )
-      }
+      <div className="BookCard__under">
+        <Button content="読む" onClick={handleRead} />
+        <Button content="貸す" onClick={() => console.log("貸す")} />
+      </div>
     </div>
   );
 };
--- lend_frontend/src/components/Store/Store.tsx
+++ lend_frontend/src/components/Store/Store.tsx
@@ -2,119 +2,52 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Screen from '../Screen/Screen';
 import Book from '../../model/book';
-import SearchIcon from '@material-ui/icons/Search';
-import BookCard from '../BookCard/BookCard';
-import StoreVerticalBookCard from './StoreVerticalBookCard/StoreVerticalBookCard';
-import CloseIcon from '@material-ui/icons/Close';
 
 const ENTRY_POINT = process.env.REACT_APP_API_ENTRYPOINT;
 
-type StoreMode = "top-page" | "search-page";
-
 const Store = () => {
-	const [rankingBooks, setRankingBooks] = useState<Book[]>([]);
-	const [discountBooks, setDiscountBooks] = useState<Book[]>([]);
-	const [allBooks, setAllBooks] = useState<Book[]>([]);
-	const [sortedBooks, setSortedBooks] = useState<Book[]>([]);
-	const [searchText, setSearchText] = useState("");
-	const [storeMode, setStoreMode] = useState<StoreMode>("top-page");
+	const [books, setBooks] = useState<Book[]>([]);
 
 	useEffect(() => {
 		axios.get(ENTRY_POINT + '/books?user_id=1').then((res) => {
 			const books = res.data.map((data: any) => Book.fromJson(data));
-			setRankingBooks(books);
-			setDiscountBooks(books);
-			setAllBooks(books);
-			setSortedBooks(books);
+			setBooks(books);
 		});
 	}, [])
-
-	useEffect(() => {
-		if (searchText.length === 0) {
-			setStoreMode("top-page");
-		} else {
-			setStoreMode("search-page");
-		}
-	}, [searchText])
-
-	const handleSearchTextChange = (searchText: string) => {
-		setSearchText(searchText);
-		setSortedBooks(allBooks.filter(book => book.name.startsWith(searchText)))
-	}
 
 	return (
 		<Screen>
 			<div className="store-page">
-				<div className="search-box-outer">
-					<input
-						type="text"
-						className="search-box"
-						placeholder="書籍検索"
-						onChange={(e) => handleSearchTextChange(e.target.value)}
-						value={searchText}
-						onSubmit={() => console.log("aa")}
-					/>
+				<div className="upper-area">
 					{
-						searchText.length != 0 ? (
-							<CloseIcon onClick={() => setSearchText("")}/>
-						) : (
-							<SearchIcon/>
+						books.map(book =>
+							<StoreBookCard key={book.id} book={book}/>
 						)
 					}
 				</div>
-				{
-					storeMode === "top-page" ? (
-						<>
-							<div className="divider"/>
-							<article className="store-page">
-								<section>
-									<h1 className="upper-title">最近人気の本</h1>
-									<div className="gallery">
-										{
-											rankingBooks.map(book =>
-												<StoreVerticalBookCard key={book.id} book={book}/>
-											)
-										}
-									</div>
-								</section>
-								<div className="divider" />
-								<section>
-									<h1 className="upper-title">割引中の本</h1>
-									<div className="gallery">
-										{
-											discountBooks.map(book =>
-												<StoreVerticalBookCard key={book.id} book={book} />
-											)
-										}
-									</div>
-								</section>
-								<div className="divider" />
-								<section>
-									<h1 className="upper-title">書籍一覧</h1>
-									{
-										allBooks.map(book =>
-											<BookCard key={book.id} book={book} type="buy"/>
-										)
-									}
-								</section>
-							</article>
-							<div className="footer-label"/>
-						</>
-					) : (
-						<section>
-							<h1 className="upper-title">検索結果 全{sortedBooks.length}件</h1>
-							{
-								sortedBooks.map(book =>
-									<BookCard key={book.id} book={book} type="buy"/>
-								)
-							}
-							<div className="footer-label">検索結果 全{sortedBooks.length}件</div>
-						</section>
-					)
-				}
+				<div className="middle-area">
+
+				</div>
+				<div className="bottom-area">
+
+				</div>
 			</div>
 		</Screen>
 	)
 }
 
 export default Store;
+
+type Props = {
+	book: Book,
+};
+const StoreBookCard = ({ book }: Props) => {
+	return (
+		<div className="store-book-card">
+			<img src={book.image}/>
+			<div>{book.name}</div>
+			<div>{book.author}</div>
+			<div>{book.info}</div>
+		</div>
+	)
+}
