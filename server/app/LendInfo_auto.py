9f52d6a4c047dc891ec45e2e5437ff8e8c80fa46
kurikinton105 <yamadakenta0101@gmail.com>
2020-09-17 17:11:09 +0900

    通知の追加

--- server/app/LendInfo_auto.py
+++ server/app/LendInfo_auto.py
@@ -1,7 +1,9 @@
 from models.config import Session
 from models.lend_info import Lend_info
+from models.own_book import Own_Book
 import datetime
 from sqlalchemy import and_, or_
+from app.AddNotification import AddNotificationInAutoReturn
 
 def IsLendInfoUpdate(lend):
     now_date = datetime.datetime.now()
@@ -28,10 +30,23 @@ def AutoUpdateLendInfo():
             if IsLendInfoUpdate(lend):
                 print("Update "+str(lend.id)+"\n")
                 lend.is_valid = False
+                # 情報をゲットする
+                borrower_id = lend.borrower_id
+                own_book_id_data = lend.own_book_id
+                user_id,book_id = GetUserIdAndBookidFromOwnBook(own_book_id_data)
+                # 自動返却の通知を追加
+                AddNotificationInAutoReturn(user_id,borrower_id,book_id)
 
     session.commit()
 
+def GetUserIdAndBookidFromOwnBook(own_book_id_data):
+    session = Session()
 
+    own_book = session.query(Own_Book).filter(
+        Own_Book.id == own_book_id_data
+    ).all()
+    for row in own_book:
+        return row.user_id,row.book_id
 
 
 
