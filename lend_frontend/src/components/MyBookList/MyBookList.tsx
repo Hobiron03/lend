e774ea731fb4bc95dcdc2f64d2066128223a154a
HOBIRON <yudainepnep@icloud.com>
2020-09-17 10:54:04 +0900

    #9 マイ本棚での所持書籍一覧取得のpost通信のIDをstateから取得したものに変更

--- lend_frontend/src/components/MyBookList/MyBookList.tsx
+++ lend_frontend/src/components/MyBookList/MyBookList.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Screen from "../Screen/Screen";
 import TabBar from "../organizations/TabBar/TabBar";
 import BookCard from "../BookCard/BookCard";
 import Book from "../../model/book";
+import AppContext from "../../contexts/AppContexts";
 
 const ENTRY_POINT = process.env.REACT_APP_API_ENTRYPOINT;
 type MyBookShellTabState = "available" | "lending";
@@ -11,68 +12,71 @@ type MyBookFilterState = "all" | "borrowing";
 const initState = "available";
 
 const MyBookList = () => {
-	const [mybooks, setMyBooks] = useState<Book[]>([]);
-	const [showBooks, setShowBooks] = useState<Book[]>([])
-	const [filterType, setFilterType] = useState<MyBookFilterState>("all");
-	const [isLoading, setIsLoading] = useState(true);
+  const { state } = useContext(AppContext);
 
-	useEffect(() => {
-		axios.get(ENTRY_POINT + '/books?user_id=1').then((res) => {
-			const books: Book[] = res.data.map((data: any) => Book.fromJson(data));
-			setMyBooks(books);
-			if (initState === "available") {
-				setShowBooks(books);
-			} else {
-				setShowBooks(books.filter(({ status }) => status === "lending"));
-			}
-			setIsLoading(false);
-		});
-	}, []);
+  const [mybooks, setMyBooks] = useState<Book[]>([]);
+  const [showBooks, setShowBooks] = useState<Book[]>([]);
+  const [filterType, setFilterType] = useState<MyBookFilterState>("all");
+  const [isLoading, setIsLoading] = useState(true);
 
-	const handleTabChange = (tabState: MyBookShellTabState) => {
-		if(tabState === "available"){
-			setShowBooks(mybooks);
-		}else{
-			setShowBooks(mybooks.filter(({ status }) => status === "lending"));
-		}
-	}
+  useEffect(() => {
+    axios.get(ENTRY_POINT + `/books?user_id=${state.user.id}`).then((res) => {
+      const books: Book[] = res.data.map((data: any) => Book.fromJson(data));
+      setMyBooks(books);
+      if (initState === "available") {
+        setShowBooks(books);
+      } else {
+        setShowBooks(books.filter(({ status }) => status === "lending"));
+      }
+      setIsLoading(false);
+    });
+  }, [state.user.id]);
 
-	const handleFilterChange = (type: MyBookFilterState) => {
-		setFilterType(type);
-	}
+  const handleTabChange = (tabState: MyBookShellTabState) => {
+    if (tabState === "available") {
+      setShowBooks(mybooks);
+    } else {
+      setShowBooks(mybooks.filter(({ status }) => status === "lending"));
+    }
+  };
 
-	useEffect(() => {
-		if(filterType === "all"){
-			setShowBooks(mybooks);
-		}else{
-			setShowBooks(mybooks.filter(book => book.status === "borrowing"));
-		}
-	}, [filterType, mybooks])
+  const handleFilterChange = (type: MyBookFilterState) => {
+    setFilterType(type);
+  };
 
-	return (
-		<Screen>
-			<TabBar initState={initState} onTabChange={handleTabChange}/>
-			<div className="mybooklist-body">
-			<div className="mybooklist-select-outer">
-				<select value={filterType} onChange={e => handleFilterChange(e.target.value as MyBookFilterState)}>
-					<option value="all">今読める本全て</option>
-					<option value="borrowing">借りている本</option>
-				</select>
-			</div>
-			{
-				showBooks.length === 0 ? !isLoading && (
-					<div className="empty-message">
-						対象の本はありません
-					</div>
-				):(
-					showBooks.map(mybook =>
-						<BookCard key={mybook.id} book={mybook}/>
-					)
-				)
-			}
-			</div>
-		</Screen>
-	)
-}
+  useEffect(() => {
+    if (filterType === "all") {
+      setShowBooks(mybooks);
+    } else {
+      setShowBooks(mybooks.filter((book) => book.status === "borrowing"));
+    }
+  }, [filterType, mybooks]);
+
+  return (
+    <Screen>
+      <TabBar initState={initState} onTabChange={handleTabChange} />
+      <div className="mybooklist-body">
+        <div className="mybooklist-select-outer">
+          <select
+            value={filterType}
+            onChange={(e) =>
+              handleFilterChange(e.target.value as MyBookFilterState)
+            }
+          >
+            <option value="all">今読める本全て</option>
+            <option value="borrowing">借りている本</option>
+          </select>
+        </div>
+        {showBooks.length === 0
+          ? !isLoading && (
+              <div className="empty-message">対象の本はありません</div>
+            )
+          : showBooks.map((mybook) => (
+              <BookCard key={mybook.id} book={mybook} />
+            ))}
+      </div>
+    </Screen>
+  );
+};
 
 export default MyBookList;
